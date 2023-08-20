1dc8966c6eb971d5049f7f408ee95aace1ca87da
Paul Llanque <llanquepol@gmail.com>
2023-08-19 19:44:17 -0500

    [FIX] ref autopopulation in models fixed

--- src/models/Album.model.js
+++ src/models/Album.model.js
@@ -1,12 +1,18 @@
+const { strictTransportSecurity } = require('helmet');
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const AlbumSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  year: { type: Number, required: true },
+  year: { type: Date, required: true },
   image: { type: String },
-  artist: { type: Schema.Types.ObjectId, ref: 'Artist' },
+  artist: {
+    type: Schema.Types.ObjectId,
+    ref: 'Artist',
+    required: true,
+    autopopulate: true,
+  },
 });
 
 AlbumSchema.plugin(require('mongoose-autopopulate'));
--- src/models/Song.model.js
+++ src/models/Song.model.js
@@ -6,7 +6,12 @@ const SongSchema = new Schema({
   name: { type: String, required: true },
   duration: { type: Number, required: true },
   file: { type: String },
-  album: { type: Schema.Types.ObjectId, ref: 'Album' },
+  album: {
+    type: Schema.Types.ObjectId,
+    ref: 'Album',
+    required: true,
+    autopopulate: true,
+  },
 });
 
 SongSchema.plugin(require('mongoose-autopopulate'));
